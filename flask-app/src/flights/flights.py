5a82012596046e9e914bae2b599c22c754081180
brianreicher <reicher.b@northeastern.edu>
2022-12-07 01:13:48 -0500

    Revamping DB

--- flask-app/src/flights/flights.py
+++ flask-app/src/flights/flights.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
+import random
 
 
 flights = Blueprint('flights', __name__)
@@ -12,7 +13,7 @@ def get_products():
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for a list of products
-    cursor.execute('select * from departures, returns')
+    cursor.execute('select * from flights')
 
     # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -30,3 +31,40 @@ def get_products():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+"""
+# Add a new user
+@flights.route('/flights/add-flight', methods=['POST'])
+def add_flight():
+    cursor = db.get_db().cursor()
+
+    date_purchased: str = request.form['date_purchased']
+    airline = request.form['airline']
+    country = request.form['country']
+    continent = request.form['continent']
+    is_layover = request.form['is_layover']
+    airport_code = request.form['airport_code']
+    purchased_price = request.form['purchased_price']
+    current_price = request.form['current_price']
+    asking_price = request.form['asking_price']
+    for_sale = request.form['for_sale']
+
+    dep_ret: str = request.form['dep_ret'].lower()
+    
+    id_tag = 'r_num'
+
+    if dep_ret == 'departures':
+        id_tag = 'd_num'
+
+
+    idNum: int =random.randint(100,10000)
+
+    insert_stmt: str = (
+                        f" INSERT INTO {dep_ret} ({id_tag}, date_purchased, airline, country, continent, is_layover, airport_code, purchased_price, current_price, asking_price, for_sale) "
+                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        )
+    data: tuple = (idNum, date_purchased, airline, country, continent, is_layover, airport_code, purchased_price, current_price, asking_price, for_sale)
+    cursor.execute(insert_stmt, data)
+    db.get_db().commit()
+    return (f'<h1>Added new flight {airline} {airport_code}: id={idNum}')
+"""
